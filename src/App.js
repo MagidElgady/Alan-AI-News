70686495c830a5029b80d41e199a14da85f0e25c
Magid Elgady <magid.elgady@outlook.com>
2020-08-23 16:52:21 +0100

    added API key in App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React, { useEffect } from 'react';
 // Adds Alan AI to program
 import alanBtn from '@alan-ai/alan-sdk-web';
 
+// API key from alan.app
+const alanKey = '0fa35faefbafc72ef792ac6aaac603472e956eca572e1d8b807a3e2338fdd0dc/stage';
+
 const App = () => {
 
-    useEffect(() => { }, [])
+    // If [] is empty, usEffect will be called once
+    useEffect(() => {
+        alanBtn({
+            // API key
+            key: alanKey,
+        })
+    }, [])
 
 
     return (
