f8ff14b0ceea970a39bf78cada7d84f01fac1e42
Magid Elgady <magid.elgady@outlook.com>
2020-08-23 16:45:29 +0100

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // Adds Alan AI to program
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 const App = () => {
+
+    useEffect(() => { }, [])
+
+
     return (
         <div>
             <h1>Alan AI News Application</h1>
