226b67b7beff4de7badcd3f09d4fff1ac2fd145d
Magid Elgady <magid.elgady@outlook.com>
2020-08-25 13:51:37 +0100

    added material ui

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 import NewsCard from '../NewsCard/NewsCard';
 
+// Used for styling our cards
+// Grid is for layout, Grow is for animation
+// and Typography is for font style
+import { Grid, Grow, Typography } from '@material-ui/core';
+
 const NewsCards = ({ articles }) => {
     return (
-        <div>
+        <Grow>
             {/* Loops through all news articles */}
             {articles.map((articles, i) => (
                 <NewsCard />
