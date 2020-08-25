476392fe1e01207702cbc3e6d3b40666f57ff903
Magid Elgady <magid.elgady@outlook.com>
2020-08-25 15:14:06 +0100

    type error fixed, styles.js should be in NewsCards

---
+++ src/components/NewsCards/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+// Uses JS to style instead of CSS
+export default makeStyles({
+    container: {
+        padding: '0 5%',
+        width: '100%',
+        margin: 0,
+
+    }
+});
+
