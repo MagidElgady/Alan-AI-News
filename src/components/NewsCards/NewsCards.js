876e28bd825b8e9cca1828793cb8c94c40a007b1
Magid Elgady <magid.elgady@outlook.com>
2020-08-25 15:09:50 +0100

    type error with line 11 in newscards.js

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -13,11 +13,11 @@ const NewsCards = ({ articles }) => {
     return (
         <Grow in >
             {/* classes.container imports styles.js code */}
-            <Grid classname={classes.container} container alignItems="stretch" spacing={3}>
+            <Grid classname={classes.container} container alignItems="stretch">
                 {/* Loops through all news articles */}
                 {articles.map((article, i) => (
                     // Sets sizes for each device e.g. 12 spaces = 1 card for xsmall devices
-                    <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }} >
+                    <Grid key={i} item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }} >
                         <NewsCard article={article} i={i} />
                     </Grid>
 
