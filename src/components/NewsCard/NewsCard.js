b039773efbcbebeae54c6b1524563af9edf90fd8
Magid Elgady <magid.elgady@outlook.com>
2020-08-25 16:51:01 +0100

    added styling to intro screen of app

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -3,25 +3,29 @@ import React from 'react';
 // Used to style each NewsCard
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 
+import useStyle from './styles';
+
 // Creates card for each News Article with all the info we want
 const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i }) => {
+    const classes = useStyle();
+
     return (
-        <Card>
-            {/* Clickable area */}
-            <CardActionArea>
+        <Card className={classes.card}>
+            {/* News Card will open article in new page when clicked on*/}
+            <CardActionArea href={url} target="_blank">
                 {/* Displays image of news company or placeholder image */}
-                <CardMedia image={urlToImage || 'https://www.indiafellow.org/blog/wp-content/uploads/2020/05/Marketplace-Lending-News.jpg'} />
-                <div>
+                <CardMedia className={classes.media} image={urlToImage || 'https://www.indiafellow.org/blog/wp-content/uploads/2020/05/Marketplace-Lending-News.jpg'} />
+                <div className={classes.details}>
                     {/* Shows date article was published */}
                     <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
                     <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
                 </div>
-                <Typography gutterBottom variant="h5">{title}</Typography>
+                <Typography className={classes.title} gutterBottom variant="h5">{title}</Typography>
                 <CardContent>
                     <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
                 </CardContent>
             </CardActionArea >
-            <CardActions>
+            <CardActions className={classes.cardActions}>
                 <Button size="small" color="primary">Learn More</Button>
                 <Typography variant="h5" color="textSecondary">{i + 1}</Typography>
             </CardActions>
--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -8,14 +8,43 @@ import { Grid, Grow, Typography } from '@material-ui/core';
 import useStyles from './styles.js';
 import NewsCard from '../NewsCard/NewsCard';
 
+const infoCards = [
+    { color: '#00838f', title: 'Latest News', text: 'Give me the latest news' },
+    { color: '#1565c0', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology News' },
+    { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with the PlayStation 5' },
+    { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN' },
+];
+
 const NewsCards = ({ articles }) => {
     // Sets style function to 
     const classes = useStyles();
 
+    // If no new articles, display this
+    if (!articles.length) {
+        return (
+            <Grow in >
+                {/* classes.container imports styles.js code */}
+                <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                    {infoCards.map((infoCard) => (
+                        <Grid item xs={12} sm={6} md={4} lg={3} className={classes.info}>
+                            <div className={classes.card} style={{ backgroundColor: infoCard.color }}>
+                                <Typography variant="h5">{infoCard.title}</Typography>
+                                {/* If infoCard has no info, show following typography */}
+                                {infoCard.info ? (<Typography variant="h6"><strong> {/* Splits title into array and grabs last value in e.g. categories or terms */} {infoCard.title.split(' ')[2]}:</strong><br />{infoCard.info}{/* If info isn't found, return null */}</Typography>) : null}
+                                <Typography variant="h6">Try saying: <br /> <i>{infoCard.text}</i></Typography>
+                            </div>
+
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grow>
+        );
+    }
+
     return (
         <Grow in >
             {/* classes.container imports styles.js code */}
-            <Grid className={classes.container} container alignItems="stretch">
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
                 {/* Loops through all news articles */}
                 {articles.map((article, i) => (
                     // Sets sizes for each device e.g. 12 spaces = 1 card for xsmall devices
