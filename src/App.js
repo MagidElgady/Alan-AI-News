40e0fdfdb9667e669490364d07c56195b7331843
Magid Elgady <magid.elgady@outlook.com>
2020-08-25 13:29:50 +0100

    added useState with newsArticles and setArticles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 // Adds Alan AI to program
 import alanBtn from '@alan-ai/alan-sdk-web';
 
@@ -6,6 +6,9 @@ import alanBtn from '@alan-ai/alan-sdk-web';
 const alanKey = '0fa35faefbafc72ef792ac6aaac603472e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+
+    // Gives access to newsArticles that can be displayed later
+    const [newsArticles, setNewsArticles] = useState([]);
 
     // If [] is empty, usEffect will be called once
     useEffect(() => {
@@ -16,7 +19,7 @@ const App = () => {
             // will pop up on screen
             onCommand: ({ command, articles }) => {
                 if (command === 'newHeadlines') {
-                    console.log(articles);
+                    setNewsArticles(articles);
                 }
             }
         })
